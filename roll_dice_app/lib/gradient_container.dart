d32d290b1ab0965f58a0877f3b09f519b84915a6
einalem4 <melanie_hornstein@hotmail.com>
2025-03-10 21:23:34 -0600

    Add dice images and implement DiceRoller component

--- roll_dice_app/lib/gradient_container.dart
+++ roll_dice_app/lib/gradient_container.dart
@@ -1,26 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:roll_dice_app/styled_text.dart';
+import 'package:roll_dice_app/dice_roller.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple({super.key})
+    : color1 = Colors.deepPurple,
+      color2 = Colors.indigo;
+
+  final Color color1;
+  final Color color2;
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 66, 58, 183),
-            Color.fromARGB(255, 58, 91, 183),
-          ],
+          colors: [color1, color2],
           begin: startAlignment,
           end: endAlignment,
         ),
       ),
-      child: const Center(child: StyledText('Hello World!')),
+      child: Center(child: DiceRoller()),
     );
   }
 }
